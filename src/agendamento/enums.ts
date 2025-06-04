64f89ba9f46ad6a60089e4c4e32a2c23e990fde8
Victor <paulo9.victor.silva@gmail.com>
2025-06-04 00:22:34 -0300

    feat: add enums for AtendimentoTipo and StatusAgendamento

---
+++ src/agendamento/enums.ts
@@ -0,0 +1,10 @@
+export enum AtendimentoTipo {
+  PRIORIDADE = 'Prioridade',
+  AGENDADO = 'Agendado',
+  ORDEM_DE_CHEGADA = 'Ordem de Chegada',
+}
+
+export enum StatusAgendamento {
+  PENDENTE = 'Pendente',
+  CONCLUIDO = 'Concluído',
+}
